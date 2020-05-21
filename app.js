5929785458efe5d16b044366905556548366d59a
deepak <deepak@brownveg>
2020-05-21 08:58:26 -0700

    fist commit

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const request = require('request')
+const productid = process.argv[2]
 
-const geocode = (sku, callback) => {
-    const url = 'https://app.ecwid.com/api/v3/16838918/products?sku='+sku+'&enabled=true&token=public_k4WsbJ2dkwGC4rnyiWVZ3zyFKqbnfjFV'
-    
-    request({ url: url, json: true }, (error, response) => {
-        if(response.body.total==0){
-            callback('product not avaible',undefined)
-                
-            }  
-            else{
-                callback(undefined, {
-                    product: response.body.items[0].name,
-                    price: response.body.items[0].price
-                
-                })
-           
+const order = (sku,callback) =>{
+    const url ='https://app.ecwid.com/api/v3/16838918/products?sku='+sku+'&token=secret_1pudxM5Phxy1WwhJWX2cqPRvXV68sc3X'
+request({url: url, json:true},(error, response) => {
+    if(response.body.total==0){
+        callback('Please enter valid order no.',undefined)
+            
+        }  
 
-            } 
-         })
+        else if(response.body.errorMessage){
 
- }
+            callback('Please enter valid order no.',undefined)
+        
+        }
+        else{
+            callback(undefined, {
+                Product: response.body.items[0].name,
+               
+                price: response.body.items[0].price
+            
+            })
+       
 
-geocode('0004', (error, data) => {
-        console.log(data)
-        console.log(error)
+        }
+    })
+
+}
+
+order(productid ,(error, data) => {
+       console.log(data)
+       console.log(error)
 })
---
+++ web.js
@@ -0,0 +1,13 @@
+const request = require('request')
+
+
+    const url ='https://webhook.site/a307ede7-5c22-4aff-9d59-e4d6991ab1bd'
+request({url: url, json:true},(response) => {
+    console.log(response)
+
+})
+
+    
+   
+
+
